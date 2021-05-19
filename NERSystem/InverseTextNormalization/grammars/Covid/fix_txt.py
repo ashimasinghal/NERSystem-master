06beaa710efbe86a48f2650c306b0edc6f23ee4f
whopriyam <priyambasu16@gmail.com>
2021-05-19 16:51:55 +0530

    New symptoms file added

--- NERSystem/InverseTextNormalization/grammars/Covid/fix_txt.py
+++ NERSystem/InverseTextNormalization/grammars/Covid/fix_txt.py
@@ -1,14 +1,17 @@
-crimefile = open('covid_sym_tracker.txt', 'r')
+crimefile = open('covid_symptom.txt', 'r')
 yourResult = [line.split('\n') for line in crimefile.readlines()]
 
 inList = []
+c = 0
 for item in yourResult:
-	inList.append(item[0])
+	c = c+1
+	if c%2==0:
+		inList.append(item[0])
 
 #print (inList)
 
 inList2 = [i for i in inList if i != "****************************************"]
 inList2 = list(set(inList2))
-with open("covid_sym_track.txt", 'w') as output:
+with open("covid_symptom_cleaned.txt", 'w') as output:
     for row in inList2:
         output.write(str(row) + '\n')
