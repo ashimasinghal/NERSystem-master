c8001442b1425d28908dea99b362c77a931faf96
whopriyam <priyambasu16@gmail.com>
2021-05-19 18:47:38 +0530

    Address covid related templates added

--- NERSystem/InverseTextNormalization/grammars/Covid/fix_txt.py
+++ NERSystem/InverseTextNormalization/grammars/Covid/fix_txt.py
@@ -1,3 +1,6 @@
+import num2words
+import re
+
 crimefile = open('covid_symptom.txt', 'r')
 yourResult = [line.split('\n') for line in crimefile.readlines()]
 
@@ -6,6 +9,7 @@ c = 0
 for item in yourResult:
 	c = c+1
 	if c%2==0:
+		item[0] = re.sub(r"(\d+)", lambda x: num2words.num2words(int(x.group(0))), item[0])
 		inList.append(item[0])
 
 #print (inList)
